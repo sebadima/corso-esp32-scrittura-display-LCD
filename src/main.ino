4cf33b8f567a83f09f868a5ee59c5dfaade9cb5c
sebadima <lenovo@sebadima.com>
2024-02-22 12:17:31 +0100

    sec commit

---
+++ src/main.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+#define I2C_SDA 23
+#define I2C_SCL 18
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() 
+{
+  Serial.begin(115200);
+  Wire.begin(I2C_SDA, I2C_SCL, 10000); 
+  lcd.init(); 
+  lcd.clear();
+  lcd.backlight();
+  lcd.setCursor(0,0);  
+  lcd.print("Inizio procedura"); 
+ 
+
+  for (int ix=1; ix<=10; ix++) 
+  {
+    lcd.clear();
+    Serial.println(ix);  
+    delay(1000);  
+  }
+
+}
+
+void loop() 
+{
+}
